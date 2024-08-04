2e656725c3e210d986b060842f9a5903f218fded
Emre Deveci <devecie03@gmail.com>
2024-08-04 22:00:05 +0300

    Update

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,10 +6,13 @@ extends Node2D
 
 func _ready():
 	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		trap.connect("touched_player", _on_saw_touched_player)
 
 func _on_death_zone_body_entered(body):
 	reset_player()
 
+# Karakterin yeniden dogma noktasi
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start_position.global_position
