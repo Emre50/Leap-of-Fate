4d49bbed28dad5e64810f1c4e63177aa27194523
Emre Deveci <devecie03@gmail.com>
2024-08-04 21:59:27 +0300

    Update
    character drift problem solved

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,26 +1,40 @@
 extends CharacterBody2D
 class_name Player
 
-var speed = 5
+var speed = 200
 @export var gravity = 400
-@export var jump_force = 200
+@export var jump_force = 300
+@export var friction = 10
 @onready var animated_sprite = $AnimatedSprite2D
 
-
 func _physics_process(delta):
-	if is_on_floor()==false:
-		velocity.y += gravity*delta
-	
-	if Input.is_action_just_pressed("jump") && is_on_floor():
+	# Yerçekimi etkisi
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Zıplama
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		jump(jump_force)
 	
+	# Hareket girişi
 	var direction = Input.get_axis("left", "right")
+	
+	# Karakterin yönünü ayarlama
 	if direction != 0:
 		animated_sprite.flip_h = (direction == -1)
-	velocity.x += direction * speed 
 	
+	# Hareket uygulama
+	velocity.x = direction * speed
+	
+	# Sürtünmeyi uygula
+	if direction == 0 and is_on_floor():
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+	
+
+	# Animasyonları güncelle
 	update_animations(direction)
 	
+	# Hareketi uygula
 	move_and_slide()
 
 func jump(force):
@@ -35,5 +49,3 @@ func update_animations(direction):
 	else:
 		if velocity.y < 0:
 			animated_sprite.play("jump")
-
-
