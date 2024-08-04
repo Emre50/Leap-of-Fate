e36a519a486c4dd7593e1f8a79f4b8ad0ecc7306
Emre Deveci <devecie03@gmail.com>
2024-08-04 17:46:44 +0300

    Update
    Traps and traps' animations added

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
 @onready var start_position = $Start
+@onready var player = $Player
 
 func _on_death_zone_body_entered(body):
-	body.velocity = Vector2.ZERO
-	body.global_position = start_position.global_position
+	reset_player()
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start_position.global_position
+
+func _on_saw_touched_player():
+	reset_player()
+
